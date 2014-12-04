fd5920f4e9979573def27fb5270a88c06139a976
Harishwar <harish.tpl@gmail.com>
2014-12-03 16:21:15 -0800

    Session Handling - Santhosh

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,11 +2,12 @@ module.exports = router;
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var sess;
 if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
     localStorage = new LocalStorage('./scratch');
 }
-//var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 var resRows = "";
 
 /* GET home page. */
@@ -17,7 +18,8 @@ router.get('/', function(req, res) {
 });
 
 router.get('/viewmodels', function(req, res) {
-    if (req.session.userName) {
+    console.log("session" + sess.userName);
+    if (sess.userName) {
         res.render('viewmodels', {
             title: 'Express'
         });
@@ -101,13 +103,6 @@ router.post('/register', function(req, res) {
     var username = req.body.userName_r;
     var pass = req.body.pass_r;
     var name = req.body.flname;
-    if (req.session.userName) {
-        res.render("home", {
-            title: 'Express'
-        });
-        return;
-    }
-
     if (username == undefined || username == "" || pass == undefined || pass == "") {
 
         res.render("index", {
@@ -116,36 +111,37 @@ router.post('/register', function(req, res) {
         return;
     }
     var status = 200;
-    // bcrypt.genSalt(10, function(err, salt) {
-    //  bcrypt.hash(pass, salt, function(err, hash) {
-    // Store hash in your ******** DB.
-    var connection = mysql.createConnection({
-        host: '*******.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
-        user: 'root',
-        ********: '********',
-        database: 'cmpe297'
-    });
-    connection.connect();
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(pass, salt, function(err, hash) {
+            // Store hash in your password DB.
+            var connection = mysql.createConnection({
+                host: 'sanatom.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
+                user: 'root',
+                password: 'password',
+                database: 'cmpe297'
+            });
+            connection.connect();
 
-    var inserttablequery = "INSERT INTO Registration(username,********, name) VALUES ('" + username + "','" + pass + "','" + name + "')";
-    console.log(inserttablequery);
-    connection.query(inserttablequery, function(error, rows, fields) {
-        if (error) {
-            return console.log(error);
-            res.send(error.body)
-            status = 500;
-            return;
-        }
-    });
+            var inserttablequery = "INSERT INTO Registration(username,password, name) VALUES ('" + username + "','" + hash + "','" + name + "')";
+            console.log(inserttablequery);
+            connection.query(inserttablequery, function(error, rows, fields) {
+                if (error) {
+                    return console.log(error);
+                    res.send(error.body)
+                    status = 500;
+                    return;
+                }
+            });
 
-    //});
-    //});
+        });
+    });
     if (status == 200) {
         //localStorage.setItem("session_key","1");
         //localStorage.setItem("username",username);
         sess = req.session;
         sess.userName = username;
-        res.render("home", {
+        console.log("sess username" + sess.userName);
+        res.render("index", {
             title: "Express"
         });
     } else {
@@ -159,12 +155,6 @@ router.post('/register', function(req, res) {
 router.post('/login', function(req, res) {
     var username = req.body.userName;
     var pass = req.body.pass;
-    if (req.session.userName) {
-        res.render("home", {
-            title: 'Express'
-        });
-        return;
-    }
     if (username == undefined || username == "" || pass == undefined || pass == "") {
         res.render("index", {
             title: 'Express'
@@ -173,15 +163,15 @@ router.post('/login', function(req, res) {
     }
 
     var connection = mysql.createConnection({
-        host: '*******.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
+        host: 'sanatom.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
         user: 'root',
-        ********: '********',
+        password: 'password',
         database: 'cmpe297'
     });
     var status;
     connection.connect();
     console.log(connection);
-    var queryString = "select username,******** from Registration where username = '" + req.session.userName + "'";
+    var queryString = "select username,password from Registration where username = '" + username + "'";
     console.log(queryString);
     connection.query(queryString, function(err, rows, fields) {
         console.log("err" + err);
@@ -192,35 +182,36 @@ router.post('/login', function(req, res) {
                 title: 'Express'
             });
         }
-        var passHash = resRows[0].********;
+        var passHash = resRows[0].password;
         console.log(passHash);
-        //  bcrypt.compare(pass, passHash, function(err, result) {
-        //if(result)
-        if (passHash == pass) {
-            sess = req.session;
-            sess.userName = username;
-            res.render("home", {
-                title: 'Express'
-            });
-        } else {
+        bcrypt.compare(pass, passHash, function(err, result) {
+            if (result) {
+                //if (passHash == pass) {
+                sess = req.session;
+                sess.userName = username;
+                res.render("home", {
+                    title: 'Express'
+                });
+            } else {
 
-            res.render("index", {
-                title: 'Express'
-            });
-            // res.json({"error":"2", "message":"incorrect credentials"});
-        }
+                res.render("index", {
+                    title: 'Express'
+                });
+                // res.json({"error":"2", "message":"incorrect credentials"});
+            }
+        });
+
     });
+    console.log(resRows);
 
 });
-console.log(resRows);
-//});
 
 router.get('/notes', function(req, res) {
     resRows = "error";
     var connection = mysql.createConnection({
-        host: '*******.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
+        host: 'sanatom.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
         user: 'root',
-        ********: '********',
+        password: 'password',
         database: 'cmpe297'
     });
     var status;
@@ -251,9 +242,9 @@ router.post('/notes', function(req, res) {
         var width = objArray[i].width;
         var height = objArray[i].height;
         var connection = mysql.createConnection({
-            host: '*******.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
+            host: 'sanatom.cvigtrqbp4hp.us-west-1.rds.amazonaws.com',
             user: 'root',
-            ********: '********',
+            password: 'password',
             database: 'cmpe297'
         });
         var status;
